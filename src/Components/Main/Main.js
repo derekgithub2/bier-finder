3d11bc5c91bc178f230973d1d5a3309c74c2d05e
Derek Yeh <derek.yeh91@gmail.com>
2023-03-01 11:36:28 -0800

    Initial commit

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Main.css'
+import Form from '../Form/Form'
+import CardContainer from '../Card/Card'
+
+const Main = () => {
+
+    return (
+        <div className='main'>
+            <p>Form/Search bar goes here</p>
+            {/* <Form /> */}
+            <p>Card Container goes here</p>
+            {/* <CardContainer /> */}
+        </div>
+    )
+}
+
+export default Main;
